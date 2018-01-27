420890efe221875e98cf6772248d88f76d1f9e06
William Bundy <william.tc.bundy@gmail.com>
2018-01-26 21:56:33 -0800

    Changed some settings that hadn't been tested

--- example_usage.bat
+++ example_usage.bat
@@ -6,9 +6,9 @@ set sspl=example_files\SourceSerifPro-Light.otf
 set sspb=example_files\SourceSerifPro-Bold.otf
 
 rem                                       width height scale x y  pxrange render
-%wiggle% fontgen %sspr% sspr.wfi sspr.png 128   96     4     8 4  4       1
-%wiggle% fontgen %sspl% sspl.wfi sspl.png 128   96     4     8 4  4       1
-%wiggle% fontgen %sspb% sspb.wfi sspb.png 128   96     4     8 4  4       1
+%wiggle% fontgen %sspr% sspr.wfi sspr.png 128   96     4     8 4  8       1
+%wiggle% fontgen %sspl% sspl.wfi sspl.png 128   96     4     8 4  8       1
+%wiggle% fontgen %sspb% sspb.wfi sspb.png 128   96     4     8 4  8       1
 
 %wiggle% atlas game_atlas.png 2048 ^
 	example_files\sprites.png 0 ^
